9e2f6e32fd778753b3175b60b5f6f3edfd9f35dd
Eric Horodyski <horodyski@ionic.io>
2020-04-16 17:53:12 -0400

    Add simple "find all" query

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contacts } from '@ionic-enterprise/contacts/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private contacts: Contacts) { }
+
+  async ngOnInit() {
+    const contacts = await this.contacts.find(['*']);
+    console.log(contacts);
+  }
 
 }
